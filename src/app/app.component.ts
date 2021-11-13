9e43fbc08944014d1d85292fb2ac40ae5819f7ec
enacheandrei7 <enache.andrei.george@gmail.com>
2021-11-13 18:19:36 +0200

    Deleted the premade page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Landing-page-DSS';
+  title: string = 'Landing-page-DSS';
 }
